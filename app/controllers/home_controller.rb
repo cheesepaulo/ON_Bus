9e1777d7dcb78a2979e542013caeda843c7e2003
Paulo Cesar <pcdsjr@gmail.com>
2017-05-14 19:57:37 -0500

    fix route and reafctoring home#controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,12 @@ require 'json'
 class HomeController < ApplicationController
 
   def posicao_veiculos
-    if !params[:linha].blank
+    if params[:linha].present?
       retorno = APIPosicao::posicao_veiculos params[:linha]
       @hr_ref = JSON.parse(retorno)['hr']
       @veiculos =  JSON.parse(retorno)['vs']
-      
+
       render json: response
-    end  
+    end
   end
 end
