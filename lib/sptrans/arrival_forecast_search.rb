39cb2438b614b63bce51ed2e8a0fbf9ab1023017
Paulo Cesar <pcdsjr@gmail.com>
2017-05-16 11:43:11 -0500

    remove unecessary new for http client

--- lib/sptrans/arrival_forecast_search.rb
+++ lib/sptrans/arrival_forecast_search.rb
@@ -19,11 +19,9 @@ module SPTrans
       end
     end
 
-    def self.getBusPosition(line_code)
+    def getBusPosition(line_code)
     # Receives a line and returns a list with all vehicles and their lat / long positions
     begin
-        @client = HTTPClient.new
-        SPTrans.connect(@client)
         response = @client.get "#{API_URL}/Posicao?codigoLinha=#{line_code}", {}
         return JSON.parse(response.body)
       rescue Exception => e
