67da489ae04611a92bead8149ecfc2a5032dee65
Rafael Aquino <rafael.acerqueirah@gmail.com>
2017-05-16 20:38:14 -0300

    Add environment variables

--- lib/sptrans/api_connection.rb
+++ lib/sptrans/api_connection.rb
@@ -1,10 +1,7 @@
 module SPTrans
-  API_URL = "http://api.olhovivo.sptrans.com.br/v0"
-  TOKEN = "5e20a6e514afa82121420017cbaf867bc2fca8b13c30f76b3c179f42f10ae486"
-
   def self.connect(client)
     begin
-      client.post("#{API_URL}/login/autenticar?token=#{TOKEN}", {})
+      client.post("#{ENV['API_URL']}/login/autenticar?token=#{ENV['TOKEN']}", {})
     rescue Exception => e
       puts e.message
       puts e.backtrace.inspect
